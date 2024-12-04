ac38888453fb53a1d993759e1750275ecb4a71f3
Naji17Mahdi <mahdinaji754@gmail.com>
2024-12-04 23:03:33 +0330

    Add files via upload

---
+++ Untitled7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+main()
+{
+	int a , b;
+	
+	
+	cout<<"Please enter a:"<<endl;
+	cin>>a;
+	cout<<"Please enter b:"<<endl;
+	cin>>b;
+	
+	cout<<endl;
+	
+	
+	
+	if ( b < a )
+	{
+		cout<<"b is the smallest.";
+	}
+	else 
+	{
+		cout<<"a is the smallest.";
+	}
+}
